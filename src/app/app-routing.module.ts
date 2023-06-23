9f2fffa881ec4479f5882a6c778c20ad8680ea77
Cristian Tejeda <do.cristian.tr@hotmail.com>
2023-06-23 14:51:54 -0400

    fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {LoginComponent} from "./components/login/login.component";
 import {RegisterComponent} from "./components/register/register.component";
+import { ForgattenPwComponent } from './components/forgatten-pw/forgatten-pw.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'forgatten-pw', component: ForgattenPwComponent },
   { path: '**', redirectTo: '/login', pathMatch: 'full' }
 ];
 
