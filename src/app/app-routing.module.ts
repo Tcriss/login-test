a45a841e3de0d32f2f182cdc987b73e82225460f
Cristian Tejeda <do.cristian.tr@hotmail.com>
2023-07-06 11:20:39 -0400

    fixing issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./components/home/home.component";
-import {LoginComponent} from "./components/login/login.component";
-import {RegisterComponent} from "./components/register/register.component";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
 import { ForgottenPwComponent } from './components/forgatten-pw/forgotten-pw.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { AuthService } from './services/auth/auth.service';
+import { Inject } from '@angular/core';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'forgotten-pw', component: ForgottenPwComponent },
+  { 
+    path: 'home', 
+    component: HomeComponent,
+    canActivate: [() => Inject(AuthService).isLoggedIn()]
+  },
+  { 
+    path: 'login', 
+    component: LoginComponent
+  },
+  { 
+    path: 'register', 
+    component: RegisterComponent 
+  },
+  { 
+    path: 'forgotten-pw', 
+    component: ForgottenPwComponent 
+  },
+  { 
+    path: 'verify-email', 
+    component: VerifyEmailComponent
+  },
   { path: '**', redirectTo: '/login', pathMatch: 'full' }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -3,20 +3,34 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { AlertsService } from '../alerts/alerts.service';
 import { GoogleAuthProvider } from "firebase/auth";
+import { BehaviorSubject, Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  user = this.auth.currentUser;
+
+  loggedIn = new BehaviorSubject<boolean>(false);
+  loggedIn$ = this.loggedIn.asObservable();
+  private user: Observable<any> | null = null;
 
   constructor(
     private auth:AngularFireAuth,
     private router:Router,
-    private alert:AlertsService
+    private alert:AlertsService,
+    private fs: AngularFirestore
   ){}
 
-  singIn(email:string, password:string){
+  isLoggedIn(): boolean {
+    return !!this.auth.currentUser;
+  }
+
+  getUser() {
+    return this.user;
+  }
+
+  singIn(email: string, password: string){
     this.auth.signInWithEmailAndPassword(email, password).then(
       (user)=>{
         this.alert.notify('Welcome','Welcome User','success');
@@ -28,7 +42,7 @@ export class AuthService {
       });
   }
 
-  register(email:string, password:string){
+  register(email: string, password: string){
     this.auth.createUserWithEmailAndPassword(email, password).then(
       (user)=>{
         this.alert.notify('Welcome','You have just registered succesfully','success');
@@ -53,7 +67,7 @@ export class AuthService {
       });
   }
 
-  singOut(){
+  logOut(){
     this.auth.signOut().then(
       user => {
         this.alert.notify('Goodbye','I hope we see you soon','success');
@@ -64,32 +78,8 @@ export class AuthService {
         this.alert.notify(`Something went wrong ${errorCode}`,errorMessage,'error');
       });
   }
-  
-  sessionState(){
-    this.auth.onAuthStateChanged(user => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/auth.user
 
-        // The user object has basic properties such as display name, email, etc.
-        const displayName:string | null = user.displayName;
-        const email:string | null = user.email;
-        const photoURL:string | null = user.photoURL;
-        const emailVerified:boolean = user.emailVerified;
-
-        // The user's ID, unique to the Firebase project. Do NOT use
-        // this value to authenticate with your backend server, if
-        // you have one. Use User.getToken() instead.
-        const uid = user.uid;
-
-        console.log(`logged user ${displayName}`);
-      } else {
-        console.log(`User logOut`);
-      }
-    })
-  }
-
-  forgottenPw(email:string){
+  forgottenPw(email: string){
     this.auth.sendPasswordResetEmail(email)
     .then(() => {
       this.alert.notify('Password Reset','An email was sent to you your mail inbos to recover your account','info');
@@ -100,4 +90,6 @@ export class AuthService {
       this.alert.notify(`Something went wrong ${errorCode}`,errorMessage,'error');
     });
   }
+
+  verifyEmail(){}
 }
